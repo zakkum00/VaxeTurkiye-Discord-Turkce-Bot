6ad997d9558b20c175e1998774d84d317fb9efe7
xChairs <39166624+xChairs@users.noreply.github.com>
2018-05-21 00:41:57 +0300

    Add files via upload

---
+++ events/ready.js
@@ -0,0 +1,19 @@
+const chalk = require('chalk');
+const moment = require('moment');
+const Discord = require('discord.js');
+const ayarlar = require('../ayarlar.json');
+
+var prefix = ayarlar.prefix;
+
+module.exports = client => {
+  console.log(`[${moment().format('YYYY-MM-DD HH:mm:ss')}] Bot Dosyalari Korunuyor`);
+  console.log(`[${moment().format('YYYY-MM-DD HH:mm:ss')}]  Koruma Calisiyor.`);
+  console.log(`[${moment().format('YYYY-MM-DD HH:mm:ss')}] Sunucu Aktif!`);
+  client.user.setStatus("online");
+  client.user.setGame(`${prefix}komutlar | 1.0.0 Güncelleme |vaxeturkiye.epizy.com`);
+  console.log(`Hazir Sunucuyu Baslatin...`);
+  console.log(`[${moment().format('YYYY-MM-DD HH:mm:ss')}] Vaxe.`);
+  console.log(`[${moment().format('YYYY-MM-DD HH:mm:ss')}] Sunucu Aktif!`);
+				  
+				  
+};
